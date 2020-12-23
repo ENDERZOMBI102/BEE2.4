f7f8908f1ae6fc5d00bbda091191e7be974eb425
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-12-24 00:17:15 +0100

    now is almost complete and functional
    Signed-off-by: ENDERZOMBI102 <enderzombi102.end@gmail.com>

--- src/app/loadScreen_daemon.py
+++ src/app/loadScreen_daemon.py
@@ -6,137 +6,7 @@ from app.canvas import Canvas, CanvasRectangle
 import app.img as img
 
 
-class SplashScreen:
-
-	def __init__(self):
-		self.lrg = SplashScreenLarge(self)
-		self.lrg.OnClose = self.OnClose
-		self.sml = SplashScreenSmall(self)
-		self.sml.OnClose = self.OnClose
-		self.sml.SetPosition( self.lrg.GetPosition() )
-
-	def Toggle( self ):
-		if self.lrg.IsShown():
-			self.lrg.Show( False )
-			self.sml.SetPosition( self.lrg.GetPosition() )
-			self.sml.Show( True )
-		else:
-			self.sml.Show( False )
-			self.lrg.SetPosition( self.sml.GetPosition() )
-			self.lrg.Show( True )
-
-	def OnClose( self ):
-		"""
-		overwrite to get the event
-		"""
-		self.Destroy()
-
-	def Destroy( self ):
-		self.sml.Destroy()
-		self.lrg.Destroy()
-
-
-class SplashScreenSmall(wx.Frame):
-
-	screenSize: wx.Size
-	splashScreen: SplashScreen
-	initDrag: Union[Tuple[int, int], None] = None
-
-	def __init__(self, ss: SplashScreen):
-		self.screenSize = wx.ScreenDC().GetSize().Get()
-		super(SplashScreenSmall, self).__init__(
-			parent=None,
-			size=wx.Size(
-				int( min( self.screenSize[ 0 ] * 0.5, 400 ) ),
-				int( min( self.screenSize[ 1 ] * 0.5, 175 ) )
-			),
-			style=wx.BORDER_NONE | wx.FRAME_NO_WINDOW_MENU
-		)
-		self.splashScreen = ss
-		self.canvas = Canvas( parent=self )
-
-		self.canvas.SetBackgroundColour( wx.Colour( '#009678' ) )
-		buttonColor: wx.Colour = wx.Colour( '#00785A' )
-
-		# close button
-		self.close_btn = self.canvas.CreateRectangle(
-			pos=wx.Point2D( self.GetSize()[ 0 ] - 20, 0 ),
-			width=20,
-			height=20,
-			fillColor=buttonColor,
-			lineColor=buttonColor,
-			layer=0
-		)
-		self.canvas.CreateLine(
-			wx.Point( self.GetSize()[ 0 ] - 16, 4 ),
-			wx.Point( self.GetSize()[ 0 ] - 4, 16 ),
-			width=2,
-			layer=0
-		)
-		self.canvas.CreateLine(
-			wx.Point( self.GetSize()[ 0 ] - 4, 4 ),
-			wx.Point( self.GetSize()[ 0 ] - 16, 16 ),
-			width=2,
-			layer=0
-		)
-
-		# resize button
-		self.resize_btn = self.canvas.CreateRectangle(
-			pos=wx.Point2D( self.GetSize()[ 0 ] - 40, 0 ),
-			width=20,
-			height=20,
-			fillColor=buttonColor,
-			lineColor=buttonColor,
-			layer=0
-		)
-		self.canvas.CreateLine(
-			wx.Point( self.GetSize()[ 0 ] - 20 - 4, 4 ),
-			wx.Point( self.GetSize()[ 0 ] - 20 - 16, 4 ),
-			width=2,
-			layer=0
-		)
-		self.canvas.CreateLine(
-			wx.Point( self.GetSize()[ 0 ] - 20 - 4, 4 ),
-			wx.Point( self.GetSize()[ 0 ] - 20 - 4, 16 ),
-			width=2,
-			layer=0
-		)
-		self.canvas.CreateLine(
-			wx.Point( self.GetSize()[ 0 ] - 24, 4 ),
-			wx.Point( self.GetSize()[ 0 ] - 36, 16 ),
-			width=2,
-			layer=0
-		)
-		
-
-		# bind events
-		self.canvas.Bind( wx.EVT_LEFT_DOWN, self.OnMouseClick )
-		self.canvas.Bind( wx.EVT_LEFT_UP, self.OnMouseClick )
-		self.canvas.Bind( wx.EVT_MOTION, self.OnMouseMove )
-
-	def OnMouseClick( self, evt: wx.MouseEvent ):
-		if self.close_btn.GetBoundingBox().IsInside( evt.GetPosition() ) and evt.LeftIsDown():
-			self.splashScreen.Destroy()
-		elif self.resize_btn.GetBoundingBox().IsInside( evt.GetPosition() ) and evt.LeftIsDown():
-			self.splashScreen.Toggle()
-		elif evt.LeftIsDown():
-			self.initDrag = evt.GetPosition().Get()
-		else:
-			self.initDrag = None
-		if evt.LeftIsDown():
-			print( 'mouse position: ' + str( evt.GetPosition().Get() ) )
-
-	def OnMouseMove( self, evt: wx.MouseEvent ):
-		if evt.Dragging() and (self.initDrag is not None):
-			self.SetPosition(
-				wx.Point(
-					self.GetPosition().Get()[ 0 ] + (evt.GetX() - self.initDrag[ 0 ]),
-					self.GetPosition().Get()[ 1 ] + (evt.GetY() - self.initDrag[ 1 ])
-				)
-			)
-
-
-class SplashScreenLarge(wx.Frame):
+class SplashScreen(wx.Frame):
 
 	compact: bool = False
 	initDrag: Union[Tuple[int, int], None] = None
@@ -145,95 +15,106 @@ class SplashScreenLarge(wx.Frame):
 	frameLargeSize: wx.Size
 	frameCompactSize: wx.Size
 
+	largeCanvas: Canvas
+	compactCanvas: Canvas
+
 	close_btn: CanvasRectangle
 	resize_btn: CanvasRectangle
 
-	splashScreen: SplashScreen
-
-	def __init__(self, ss: SplashScreen):
+	def __init__(self):
 		self.screenSize = wx.ScreenDC().GetSize().Get()
 
-		super(SplashScreenLarge, self).__init__(
+		# sizes
+		self.frameLargeSize = wx.Size(
+			int( max( self.screenSize[ 0 ] * 0.6, 500 ) ),
+			int( max( self.screenSize[ 1 ] * 0.6, 400 ) )
+		)
+		self.frameCompactSize = wx.Size(
+			int( min( self.screenSize[ 0 ] * 0.5, 400 ) ),
+			int( min( self.screenSize[ 1 ] * 0.5, 175 ) )
+		)
+
+		super(SplashScreen, self).__init__(
 			parent=None,
-			size=wx.Size(
-				int( max( self.screenSize[ 0 ] * 0.6, 500 ) ),
-				int( max( self.screenSize[ 1 ] * 0.6, 400 ) )
-			),
+			size=self.frameLargeSize,
 			style=wx.BORDER_NONE | wx.FRAME_NO_WINDOW_MENU
 		)
 		self.CenterOnScreen()
 
-		self.splashScreen = ss
-
-		self.canvas = Canvas( parent=self )
+		self.largeCanvas = Canvas( parent=self )
+		self.compactCanvas = Canvas( parent=self )
+		self.compactCanvas.Show( False )
+		self.compactCanvas.Enable( False )
 		buttonColor: wx.Colour = wx.Colour( '#00785A' )
 
+		# LARGE CANVAS
+
 		# close button
-		self.close_btn = self.canvas.CreateRectangle(
-			pos=wx.Point2D( self.GetSize()[ 0 ] - 20, 0 ),
+		self.close_btn = self.largeCanvas.CreateRectangle(
+			pos=wx.Point( self.frameLargeSize[ 0 ] - 20, 0 ),
 			width=20,
 			height=20,
 			fillColor=buttonColor,
 			lineColor=buttonColor,
 			layer=1
 		)
-		self.canvas.CreateLine(
-			wx.Point( self.GetSize()[ 0 ] - 16, 4 ),
-			wx.Point( self.GetSize()[ 0 ] - 4, 16 ),
+		self.largeCanvas.CreateLine(
+			wx.Point( self.frameLargeSize[ 0 ] - 16, 4 ),
+			wx.Point( self.frameLargeSize[ 0 ] - 4, 16 ),
 			width=2,
 			layer=1
 		)
-		self.canvas.CreateLine(
-			wx.Point( self.GetSize()[ 0 ] - 4, 4 ),
-			wx.Point( self.GetSize()[ 0 ] - 16, 16 ),
+		self.largeCanvas.CreateLine(
+			wx.Point( self.frameLargeSize[ 0 ] - 4, 4 ),
+			wx.Point( self.frameLargeSize[ 0 ] - 16, 16 ),
 			width=2,
 			layer=1
 		)
 
 		# resize button
-		self.resize_btn = self.canvas.CreateRectangle(
-			pos=wx.Point2D( self.GetSize()[ 0 ] - 40, 0 ),
+		self.resize_btn = self.largeCanvas.CreateRectangle(
+			pos=wx.Point( self.frameLargeSize[ 0 ] - 40, 0 ),
 			width=20,
 			height=20,
 			fillColor=buttonColor,
 			lineColor=buttonColor,
 			layer=1
 		)
-		self.canvas.CreateLine(
-			wx.Point( self.GetSize()[ 0 ] - 36, 16 ),
-			wx.Point( self.GetSize()[ 0 ] - 24, 16 ),
+		self.largeCanvas.CreateLine(
+			wx.Point( self.frameLargeSize[ 0 ] - 36, 16 ),
+			wx.Point( self.frameLargeSize[ 0 ] - 24, 16 ),
 			width=2,
 			layer=1
 		)
-		self.canvas.CreateLine(
-			wx.Point( self.GetSize()[ 0 ] - 36, 16 ),
-			wx.Point( self.GetSize()[ 0 ] - 36, 4 ),
+		self.largeCanvas.CreateLine(
+			wx.Point( self.frameLargeSize[ 0 ] - 36, 16 ),
+			wx.Point( self.frameLargeSize[ 0 ] - 36, 4 ),
 			width=2,
 			layer=1
 		)
-		self.canvas.CreateLine(
-			wx.Point( self.GetSize()[ 0 ] - 24, 4 ),
-			wx.Point( self.GetSize()[ 0 ] - 36, 16 ),
+		self.largeCanvas.CreateLine(
+			wx.Point( self.frameLargeSize[ 0 ] - 24, 4 ),
+			wx.Point( self.frameLargeSize[ 0 ] - 36, 16 ),
 			width=2,
 			layer=1
 		)
 
 		# BEE logo
-		self.canvas.CreateImage(
+		self.largeCanvas.CreateImage(
 			img.png( './../images/BEE2/splash_logo.png', wxImg=True ),
 			wx.Point( 10, 10 ),
 			layer=1
 		)
 
 		# texts
-		text = self.canvas.CreateText(
+		text = self.largeCanvas.CreateText(
 			text='Better Extended Editor for Portal 2',
 			pos=wx.Point( 10, 125 ),
 			color=wx.ColourDatabase().Find( 'WHITE' ),
 			font=wx.Font( wx.FontInfo( 14 ).FaceName( 'DIN' ).Bold( True ) ),
 			layer=1
 		)
-		versionText = self.canvas.CreateText(
+		versionText = self.largeCanvas.CreateText(
 			text='Version: (dev)',
 			pos=wx.Point( 10, 145 ),
 			color=wx.ColourDatabase().Find( 'WHITE' ),
@@ -255,10 +136,10 @@ class SplashScreenLarge(wx.Frame):
 		)
 
 		# background image + shadows + gradient
-		self.canvas.CreateImage(
+		self.largeCanvas.CreateImage(
 			image=img.make_splash_screen(
-				max_width=self.GetSize().Get()[ 0 ],
-				max_height=self.GetSize().Get()[ 1 ],
+				max_width=self.frameLargeSize.Get()[ 0 ],
+				max_height=self.frameLargeSize.Get()[ 1 ],
 				base_height=80,
 				text1_bbox=text1_bbox,
 				text2_bbox=text2_bbox,
@@ -266,17 +147,95 @@ class SplashScreenLarge(wx.Frame):
 			),
 			pos=wx.Point( 0, 0 )
 		)
-		self.canvas.Bind( wx.EVT_LEFT_DOWN, self.OnMouseClick )
-		self.canvas.Bind( wx.EVT_LEFT_UP, self.OnMouseClick )
-		self.canvas.Bind( wx.EVT_MOTION, self.OnMouseMove )
+
+		# COMPACT CANVAS
+
+		# background
+		# self.compactCanvas.SetBackgroundColour( wx.Colour( '#009678' ) )
+
+		# close button
+		self.cclose_btn = self.compactCanvas.CreateRectangle(
+			pos=wx.Point( self.frameCompactSize[ 0 ] - 20, 0 ),
+			width=20,
+			height=20,
+			fillColor=buttonColor,
+			lineColor=buttonColor,
+			layer=0
+		)
+		self.compactCanvas.CreateLine(
+			wx.Point( self.frameCompactSize[ 0 ] - 16, 4 ),
+			wx.Point( self.frameCompactSize[ 0 ] - 4, 16 ),
+			width=2,
+			layer=0
+		)
+		self.compactCanvas.CreateLine(
+			wx.Point( self.frameCompactSize[ 0 ] - 4, 4 ),
+			wx.Point( self.frameCompactSize[ 0 ] - 16, 16 ),
+			width=2,
+			layer=0
+		)
+
+		# resize button
+		self.cresize_btn = self.compactCanvas.CreateRectangle(
+			pos=wx.Point( self.frameCompactSize[ 0 ] - 40, 0 ),
+			width=20,
+			height=20,
+			fillColor=buttonColor,
+			lineColor=buttonColor,
+			layer=0
+		)
+		self.compactCanvas.CreateLine(
+			wx.Point( self.frameCompactSize[ 0 ] - 20 - 4, 4 ),
+			wx.Point( self.frameCompactSize[ 0 ] - 20 - 16, 4 ),
+			width=2,
+			layer=0
+		)
+		self.compactCanvas.CreateLine(
+			wx.Point( self.frameCompactSize[ 0 ] - 20 - 4, 4 ),
+			wx.Point( self.frameCompactSize[ 0 ] - 20 - 4, 16 ),
+			width=2,
+			layer=0
+		)
+		self.compactCanvas.CreateLine(
+			wx.Point( self.frameCompactSize[ 0 ] - 24, 4 ),
+			wx.Point( self.frameCompactSize[ 0 ] - 36, 16 ),
+			width=2,
+			layer=0
+		)
+
+		# bind events
+		self.largeCanvas.Bind( wx.EVT_LEFT_DOWN, self.OnMouseClick )
+		self.largeCanvas.Bind( wx.EVT_LEFT_UP, self.OnMouseClick )
+		self.largeCanvas.Bind( wx.EVT_MOTION, self.OnMouseMove )
+		self.compactCanvas.Bind( wx.EVT_LEFT_DOWN, self.OnMouseClick )
+		self.compactCanvas.Bind( wx.EVT_LEFT_UP, self.OnMouseClick )
+		self.compactCanvas.Bind( wx.EVT_MOTION, self.OnMouseMove )
 		self.Show()
 
-	# to move the window + react to buttons
+	def Toggle( self ):
+		if self.compact:
+			self.compactCanvas.Show( False )
+			self.compactCanvas.Enable( False )
+			self.largeCanvas.Show()
+			self.SetSize( self.frameLargeSize )
+			self.compact = False
+		else:
+			self.largeCanvas.Show( False )
+			self.compactCanvas.Enable()
+			self.compactCanvas.Show()
+			self.SetSize( self.frameCompactSize )
+			self.compact = True
+
+		# to move the window + react to buttons
 	def OnMouseClick(self, evt: wx.MouseEvent):
-		if self.close_btn.GetBoundingBox().IsInside( evt.GetPosition() ) and evt.LeftDown():
-			self.splashScreen.Destroy()
-		elif self.resize_btn.GetBoundingBox().IsInside( evt.GetPosition() ) and evt.LeftDown():
-			self.splashScreen.Toggle()
+		if self.close_btn.GetBoundingBox().IsInside( evt.GetPosition() ) and evt.LeftDown() and not self.compact:
+			self.Destroy()
+		elif self.resize_btn.GetBoundingBox().IsInside( evt.GetPosition() ) and evt.LeftDown() and not self.compact:
+			self.Toggle()
+		elif self.cclose_btn.GetBoundingBox().IsInside( evt.GetPosition() ) and evt.LeftDown():
+			self.Destroy()
+		elif self.cresize_btn.GetBoundingBox().IsInside( evt.GetPosition() ) and evt.LeftDown():
+			self.Toggle()
 		elif evt.LeftDown():
 			self.initDrag = evt.GetPosition().Get()
 		else:
@@ -296,5 +255,5 @@ class SplashScreenLarge(wx.Frame):
 
 if __name__ == '__main__':
 	app = wx.App()
-	splashScreen = SplashScreen()
+	frame = SplashScreen()
 	app.MainLoop()
